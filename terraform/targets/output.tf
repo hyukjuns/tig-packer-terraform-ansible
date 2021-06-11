0bd692cabf7a74c9412aa032d41ee24a71f4ad47
namhj94 <hyukjun1994@gmail.com>
2021-06-11 19:36:19 +0900

    ansible updatE

--- terraform/targets/output.tf
+++ terraform/targets/output.tf
@@ -1,6 +1,6 @@
-output "target_Ubuntu_public_ip" {
-    value = azurerm_linux_virtual_machine.target_00_server.public_ip_address
+output "target_Ubuntu_private_ip" {
+    value = azurerm_linux_virtual_machine.target_00_server.private_ip_address
 }
-output "target_CentOS_public_ip" {
-    value = azurerm_linux_virtual_machine.target_01_server.public_ip_address
+output "target_CentOS_private_ip" {
+    value = azurerm_linux_virtual_machine.target_01_server.private_ip_address
 }
