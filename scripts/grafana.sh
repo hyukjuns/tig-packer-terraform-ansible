4a72da0971d4a762223728c94096514f6f852b6a
namhj94 <hyukjun1994@gmail.com>
2021-11-17 18:45:55 +0900

    update scripts

--- scripts/grafana.sh
+++ scripts/grafana.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Setup
+# APT Repository 설치
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-# Add this repository for stable releases:
+# APT Repository 등록
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
-# Install
+# Grafana 설치
 sudo apt-get update -y
 sudo apt-get install -y grafana
 
-# Start Server & Enable Server
+# 서비스 시작
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server
--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# install
+# Telegraf 설치
 wget https://dl.influxdata.com/telegraf/releases/telegraf_1.18.1-1_amd64.deb
 sudo dpkg -i telegraf_1.18.1-1_amd64.deb
 
-# Set Configuration
-sudo sed -i "/\[\[outputs.influxdb\]\]/a\urls = [\"http://10.0.0.4:8086\"]" /etc/telegraf/telegraf.conf
-sudo sed -i "/\[\[outputs.influxdb\]\]/a\database = \"telegrafdb\"" /etc/telegraf/telegraf.conf
+# Cofig 수정
+sudo sed -i "/\[\[outputs.influxdb\]\]/a\urls = [\"http://<DB_SERVER_IP>:8086\"]" /etc/telegraf/telegraf.conf
+sudo sed -i "/\[\[outputs.influxdb\]\]/a\database = \"<DATABASE>\"" /etc/telegraf/telegraf.conf
 
-# Start Service 
+# 서비스 시작 
 sudo systemctl daemon-reload
 sudo systemctl start telegraf.service
 sudo systemctl enable telegraf.service
